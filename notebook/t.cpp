98f45b2e57d7a5c6552bb7b896b4a468c2e4d867
Austin Conner <aconner.vu@gmail.com>
2014-10-02 01:48:36 -0500

    add cpp template file to notebook

---
+++ notebook/t.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <map>
+#include <set>
+#include <algorithm>
+#include <queue>
+#include <cassert>
+
+using namespace std;
+
+typedef vector<int> vi;
+typedef vector<vi> vvi;
+typedef pair<int,int> pii;
+typedef vector<pii> vpii;
+typedef map<int,int> mii;
+typedef map<string,int> msi;
+typedef set<int> si;
+
+int main () {
+
+  return 0;
+}
